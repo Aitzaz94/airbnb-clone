6d08558ebee5e90edc818fcf0914710ae79f2cc4
aitzaz <aitzazakmal@gmail.com>
2024-10-14 10:24:28 +0500

    property model in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_151055) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_044349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "headline"
+    t.string "name"
+    t.text "description"
+    t.string "address_2"
+    t.string "address_1"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
