729106e5d61dd305ad99af2f6cfdd5bff6b0cf81
aitzaz <aitzazakmal@gmail.com>
2024-10-23 14:32:19 +0500

    updated wishlist-js controller for index page and individual property page by adding/managing targets

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
+  static targets = ['icon', 'text']
   
-  updateWishlistStatus() {
+  updateWishlistStatus(e) {
+    e.preventDefault();
       const isUserLoggedIn = this.element.dataset.userLoggedIn;
 
       if (isUserLoggedIn === "false")
@@ -51,9 +54,12 @@ export default class extends Controller {
     .then(data => {
       console.log(data);
       this.element.dataset.wishlistId = data.id;
-      this.element.classList.remove("fill-none");
-      this.element.classList.add("fill-primary");
       this.element.dataset.status = "true";
+      this.iconTarget.classList.remove("fill-none");
+      this.iconTarget.classList.add("fill-primary");
+      if (this.textTarget) {
+        this.textTarget.innerText="Saved";
+      }
     })
     .catch(e => {
       console.log(e);
@@ -66,9 +72,12 @@ export default class extends Controller {
     })
     .then(response => {
       this.element.dataset.wishlistId = '';
-      this.element.classList.remove("fill-primary");
-      this.element.classList.add("fill-none");
       this.element.dataset.status = "false";
+      this.iconTarget.classList.remove("fill-primary");
+      this.iconTarget.classList.add("fill-none");
+      if (this.textTarget) {
+        this.textTarget.innerText="Save";
+      }
     })
     .catch(e => {
       console.log(e);
